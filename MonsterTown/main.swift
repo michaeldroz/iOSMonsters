3076da841f02a746f4d6d055b36e55433d1d3c00
Michael Droz <michael.droz@tcc-mdroz.local>
2014-11-16 10:40:14 -0800

    Initial Commit

---
+++ MonsterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Michael Droz on 11/16/14.
+//  Copyright (c) 2014 Michael Droz. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
